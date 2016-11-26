e3c180dbd20058e46b93e8912a983fa266a021a7
tasti <tasti@zakarie.com>
2016-11-26 18:23:09 -0500

    working email for jobmine 😂

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const express = require('express');
 
 const telnet = require('./telnet');
+const jobmine = require('./templates/jobmine');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -12,8 +13,21 @@ app.use(cors());
 app.use(express.static('build'))
 
 app.post('/api/jobmine', (req, res) => {
-  console.log('body: ', req.body);
-  telnet(req.body.mailFrom, req.body.rcptTo, 'THIS WORKS OMG :DDD');
+  const {
+    mailFrom,
+    rcptTo,
+    name,
+    company,
+    jobTitle,
+    link,
+  } = req.body;
+
+  const body = jobmine(name, rcptTo, company, jobTitle, link);
+console.log('-body-start-');
+console.log(body);
+console.log('-body-end-');
+  telnet('ceca@uwaterloo.ca', req.body.rcptTo, body);
+
   return res.json({"succes": "yo"});
 });
 
--- src/App.js
+++ src/App.js
@@ -3,10 +3,41 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  send = () => {
+    const mailFrom = document.getElementById('from').value;
+    const rcptTo = document.getElementById('to').value;
+    const name = document.getElementById('toname').value;
+    const company = document.getElementById('company').value;
+    const jobTitle = document.getElementById('jobtitle').value;
+    const link = document.getElementById('link').value;
+
+    fetch('http://localhost:8000/api/jobmine', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        mailFrom,
+        rcptTo,
+        name,
+        company,
+        jobTitle,
+        link,
+      })
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        Spoofify
+        <div>Spoofify</div>
+        From: <input id="from" />
+        To: <input id="to" />
+        To Name: <input id="toname" />
+        Company: <input id="company" />
+        Job Title: <input id="jobtitle" />
+        Link: <input id="link" />
+        <button onClick={this.send}>Send</button>
       </div>
     );
   }
