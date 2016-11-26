7cc94b79716b352739480e6abf0699af80d3f618
tasti <tasti@zakarie.com>
2016-11-26 17:37:07 -0500

    Basic implementation of api

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 
+const telnet = require('./telnet');
+
 const app = express();
 const PORT = process.env.PORT || 8000;
 
@@ -9,7 +11,21 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('build'))
 
-app.get('/api', (req, res) => {
+app.post('/api/jobmine', (req, res) => {
+  console.log('body: ', req.body);
+  telnet(req.body.mailFrom, req.body.rcptTo, 'THIS WORKS OMG :DDD');
+  return res.json({"succes": "yo"});
+});
+
+app.post('/api/nigerianprince', (req, res) => {
+  // console.log('body: ', req.body);
+  // telnet(req.body.mailFrom, req.body.rcptTo, 'THIS WORKS OMG :DDD');
+  // return res.json({"succes": "yo"});
+});
+
+app.post('/api', (req, res) => {
+  console.log('body: ', req.body);
+  telnet(req.body.mailFrom, req.body.rcptTo, 'THIS WORKS OMG :DDD');
   return res.json({"succes": "yo"});
 });
 
---
+++ telnet.js
@@ -0,0 +1,39 @@
+const telnet = require('telnet-client');
+
+const connection = new telnet();
+const params = {host: 'mxer.uwaterloo.ca', port: 25, shellPrompt: '********************************************************************************************************************************************'};
+
+module.exports = (mailFrom, rcptTo, body) => {
+  return connection.connect(params)
+    .then((response) => {
+      console.log(response);
+    })
+    .then((response) => {
+      console.log(response);
+      return connection.send('HELO mxer.uwaterloo.ca', {waitfor: 'you'});
+    })
+    .then((response) => {
+      console.log(response);
+      return connection.send(`MAIL FROM: ${mailFrom}`, {waitfor: 'ok'});
+    })
+    .then((response) => {
+      console.log(response);
+      return connection.send(`RCPT TO: ${rcptTo}`, {waitfor: 'ok'});
+    })
+    .then((response) => {
+      console.log(response);
+      return connection.send('DATA', {waitfor: 'itself'});
+    })
+    .then((response) => {
+      console.log(response);
+      return connection.send(`${body}\n.`, {waitfor: 'delivery'});
+    })
+    .then((response) => {
+      console.log(response);
+      return connection.send('QUIT', {waitfor: 'connection'});
+    })
+    .then((response) => {
+      console.log(response);
+      return connection.end();
+    });
+};
